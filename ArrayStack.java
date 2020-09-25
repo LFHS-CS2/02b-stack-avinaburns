907361b581cfee28132bbf547ed2506f1c25f325
avinaburns <69760248+avinaburns@users.noreply.github.com>
2020-09-25 12:53:48 -0500

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,73 +1,67 @@
 /**
- * ArrayStack should implement the Stack interface.
- * You should write your own test cases in ArrayStackTest
+ * ArrayStack should implement the Stack interface. You should write your own
+ * test cases in ArrayStackTest
  *
- * @author Avina Burns
+ * @author (your name)
  */
-public class ArrayStack<E> implements Stack<E>
-{
- Object[] stack = new Object[100];
+public class ArrayStack<E> implements Stack<E> {
+  Object[] stack = new Object[100];
 
- public boolean isEmpty() {
-   for(int i = 0; i <stack.length; i++){
-    if (stack[i]!=null)
-     return false;
- }
- return true;
- }
-
-    /** 
-     * Return the value that pop would give, without modifying
-     * the stack. TODO:  Throw a StackException if the stack is empty. 
-     */
-    public E peekTop(){
-      if(isEmpty())
-       return null;
-      for(int i = stack.length-1; i >= 0; i--){
-        if(stack[i] != null)
-          return (E)stack[i];
-      }
-      return null;
+  public boolean isEmpty() {
+    for (int i = 0; i < stack.length; i++) {
+      if (stack[i] != null)
+        return false;
     }
+    return true;
+  }
 
-    /** 
-     * Remove and return the value that has been in the stack the
-     * least time. TODO: Throw a StackException if the stack is empty. 
-     */
-    public E pop(){
-
-       Object temp = null;
-       for (int i = stack.length; i>=0; i--){
-         if (stack[i]!=null){
-             temp = stack[i];
-            stack[i]=null;
-            break;
-         }
-       }
-      return (E)temp;
-    }
-
-    
-    /** 
-     * Add the given value to the stack. 
-     */
-    public void push (E ob){
-     if (stack[stack.length-1] !=null){
-        int newlength = stack.length*2;
-        Object[] tempstack = new Object[newlength];
-        for (int i=0; i<stack.length; i++){ 
-          tempstack[i] = stack[i];
-        }
-        stack = tempstack;
+  /**
+   * Return the value that pop would give, without modifying the stack. TODO:
+   * Throw a StackException if the stack is empty.
+   */
+  public E peekTop() {
+    if (isEmpty())
+      throw new StackException();
+    for (int i = stack.length - 1; i >= 0; i--) {
+      if (stack[i] != null) {
+        return (E) stack[i];
       }
-       
+    }
+    return null;
+  }
 
-     for(int i = stack.length-1; i >= 0; i--){
-      if(stack[i] != null){
-        stack[i+1] = ob;
+  /**
+   * Remove and return the value that has been in the stack the least time. TODO:
+   * Throw a StackException if the stack is empty.
+   */
+  public E pop() {
+    if (isEmpty())
+      throw new StackException();
+
+    Object temp = null;
+    for (int i = stack.length - 1; i >= 0; i--) {
+      if (stack[i] != null) {
+        temp = stack[i];
+        stack[i] = null;
         break;
       }
     }
+    return (E) temp;
+  }
+
+  /**
+   * Add the given value to the stack.
+   */
+  public void push(E ob) {
+
+    for (int i = stack.length - 1; i >= 0; i--) {
+      if (stack[i] != null && stack.length - 1 != i) {
+        stack[i + 1] = ob;
+        break;
+      }
     }
+    if (stack[0] == null)
+      stack[0] = ob;
+  }
+
 }
- 
